d0d10226c50087f88fd87441f0bc272b808fc1a7
laskor <al.moksud@gmail.com>
2022-03-23 23:23:58 -0400

    unidirectional

---
+++ unidirectional-data-flow.js
@@ -0,0 +1,7 @@
+/* 
+
+Unidirectional data flow is a technique 
+that is mainly found in functional reactive programming. 
+It is also known as one-way data flow, which means the data has one, 
+and only one way to be transferred to other parts of the application
+*/
