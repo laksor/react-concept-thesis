f25e1745c07821b99a0b61d2acab2d90f0c2a717
laskor <al.moksud@gmail.com>
2022-03-23 23:30:58 -0400

    asynchronous

---
+++ asynchronous.js
@@ -0,0 +1,10 @@
+/* 
+
+It aims to help with handling async operations by 
+letting you wait for some code to load and declaratively 
+specify a loading state (like a spinner) while waiting. 
+It allows you to defer rendering part of your application tree 
+until some condition is met (for example, data from an endpoint 
+or a resource is loaded).
+
+*/
