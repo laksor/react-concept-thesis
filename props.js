c59d84fb4b9fb1447939769771ad6491249f8bfa
laskor <al.moksud@gmail.com>
2022-03-23 23:20:40 -0400

    props

---
+++ props.js
@@ -0,0 +1,10 @@
+/* 
+Props are arguments passed into React components. 
+Props are passed to components via HTML attributes. 
+props stands for properties. 
+*/
+
+function Welcome(props) {
+    return(<h1>Hello, {props.name}</h1>);
+}
+
